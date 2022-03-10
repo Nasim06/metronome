1512e44e0de0c2d084b56f9ca6d8f81c2bfb4cc5
Nasim06 <nasimahmed_06@hotmail.co.uk>
2022-03-10 23:51:45 +0000

    adding a stopwatch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 
 import './App.css';
+import { useStopwatch } from 'react-timer-hook';
 
 function App() {
+
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({ autoStart: true });
+
+  function timer(){
+    if(!isRunning){
+      {start};
+    }
+    else{
+      {pause};
+    }
+
+  }
+  
+
+
+
+
+
+
+
   return (
     <>
       <div className='header'>
         <h1 className='title'>Metronome</h1>
       </div>
+
+      <div className='main'>
+
+        <h1>minutes:seconds</h1>
+
+        <button onClick={timer()} >Start Tempo</button>
+
+
+
+      </div>
     
     
     </>
   );
+
 }
 
 export default App;
