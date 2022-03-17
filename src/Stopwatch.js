df355473af37d55587ef6eb753110d47e67ca445
Nasim06 <nasimahmed_06@hotmail.co.uk>
2022-03-17 20:37:31 +0000

    adding to the component

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,7 +1,27 @@
-
+import React, { useEffect, useState } from 'react'
 
 function Stopwatch(){
 
+    const [time, setTime] = useState(0)
+    const [timerOn, setTimerOn] = useState(false)
+
+    useEffect (()=> {
+        if(timerOn === true){
+            setTime(time + 1)
+        }
+    })
+    
+  
+
+
+
+    function Start (){
+        setTimerOn(true)
+    }
+
+    function Stop(){
+        setTimerOn(false)
+    }
 
 
 
