75e8a6a82b87eeec0bc934f6ee65949dbda20a7f
Nasim06 <nasimahmed_06@hotmail.co.uk>
2022-03-29 22:35:09 +0100

    changing the start and stop to one button

--- src/App.js
+++ src/App.js
@@ -7,24 +7,31 @@ function App() {
 
   const [time, setTime] = useState(0)
   const [timerOn, setTimerOn] = useState(false)
-  let startTime
   let nf = Intl.NumberFormat()
 
-useEffect(() => {
+  useEffect(() => {
 
-  let interval = null;
-  if(timerOn){
-    interval = setInterval(() => {
-      setTime(prevTime => prevTime + 10)
-    }, 10)
-  }else{
-    clearInterval(interval)
+    let interval = null;
+    if(timerOn){
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10)
+    }else{
+      clearInterval(interval)
+    }
+    
+    return () => clearInterval(interval)
+  }, [timerOn])
+
+  let playPause = () => {
+
+    if(timerOn === false){
+      setTimerOn(true)
+    }
+    else{
+      setTimerOn(false)
+    }
   }
-  
-  return () => clearInterval(interval)
-}, [timerOn])
-
-
 
   return (
     <>
@@ -34,18 +41,13 @@ useEffect(() => {
 
       <div className='main'>
 
-        <h1>{nf.format(time)}</h1>
-        <button className='startButton' onClick={() => setTimerOn(true)}>Start Tempo</button>
-        <button className='stopButton' onClick={() => setTimerOn(false)}>Stop Tempo</button>
-
-
+        <h1 className='time'>{nf.format(time)}</h1>
+        <button className='startButton' onClick={playPause}>Start Tempo</button>
 
       </div>
-    
     
     </>
   );
 
 }
-
 export default App;
